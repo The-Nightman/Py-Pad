6b3646e7c5dde8d855c4a7f6617386deb370a084
The Nightman <102193772+The-Nightman@users.noreply.github.com>
2022-11-05 01:03:38 +0000

    Update pypad.py
    fixed window name loss if open dialogue is cancelled, abbreviated imported libraries to condense line lengths

--- pypad.py
+++ pypad.py
@@ -1,7 +1,7 @@
 # pip install customtkinter
 
-import customtkinter
-import tkinter
+import customtkinter as ctk
+import tkinter as tk
 from tkinter import filedialog
 import webbrowser
 
@@ -10,7 +10,7 @@ open_file_name = False
 
 def new_file(event=None):
     global open_file_name
-    if tkinter.Event:
+    if tk.Event:
         open_file_name = False
         text_box.delete("1.0", "end")
         app.title("Untitled - Py Pad")
@@ -22,9 +22,11 @@ def new_file(event=None):
 
 def open_file(event=None):
     global open_file_name
-    if tkinter.Event:
+    if tk.Event:
         txt_file = filedialog.askopenfilename(initialdir="C:", title="Open File", filetypes=(
             ("Text Files", "*.txt"), ("All Files", "*.*")))
+        if txt_file == "":
+            txt_file = open_file_name
         if txt_file:
             open_file_name = txt_file
         title = txt_file.replace("\\", "/").split("/")
@@ -36,6 +38,8 @@ def open_file(event=None):
     else:
         txt_file = filedialog.askopenfilename(initialdir="C:", title="Open File", filetypes=(
             ("Text Files", "*.txt"), ("All Files", "*.*")))
+        if txt_file == "":
+            txt_file = open_file_name
         if txt_file:
             open_file_name = txt_file
         title = txt_file.replace("\\", "/").split("/")
@@ -47,7 +51,7 @@ def open_file(event=None):
 
 
 def save_as(event=None):
-    if tkinter.Event:
+    if tk.Event:
         txt_file = filedialog.asksaveasfilename(
             defaultextension=".*", initialdir="C:", title="Save As", filetypes=(("Text Files", "*.txt"), ("All Files", "*.*")))
         if txt_file:
@@ -69,7 +73,7 @@ def save_as(event=None):
 
 def save(event=None):
     global open_file_name
-    if tkinter.Event:
+    if tk.Event:
         if open_file_name:
             txt_file = open(open_file_name, "w")
             txt_file.write(text_box.get("1.0", "end"))
@@ -87,7 +91,7 @@ def save(event=None):
 
 def cut_text():
     global text_select
-    if tkinter.Event:
+    if tk.Event:
         text_select = app.clipboard_get()
     else:
         if text_box.selection_get():
@@ -99,7 +103,7 @@ def cut_text():
 
 def copy_text():
     global text_select
-    if tkinter.Event:
+    if tk.Event:
         text_select = app.clipboard_get()
     if text_box.selection_get():
         text_select = text_box.selection_get()
@@ -109,10 +113,10 @@ def copy_text():
 
 def paste_text():
     global text_select
-    if tkinter.Event:
+    if tk.Event:
         text_select = app.clipboard_get()
     else:
-        cursor_pos = text_box.index(tkinter.INSERT)
+        cursor_pos = text_box.index(tk.INSERT)
         text_box.insert(cursor_pos, text_select)
 
 
@@ -125,22 +129,22 @@ def toggle_wrap():
 
 def create_about_window():
     global about_image
-    about_window = tkinter.Toplevel(app)
+    about_window = tk.Toplevel(app)
     about_window.geometry(f"{350}x{220}")
     about_window.title("About Py Pad")
     about_window.resizable(False, False)
     about_window.transient(app)
-    about_window_button = customtkinter.CTkButton(
+    about_window_button = ctk.CTkButton(
         about_window, text="OK", relief="groove", corner_radius=3, command=about_window.destroy)
     about_window_button.pack(side="bottom", anchor="e", padx=10, pady=10)
-    about_image = tkinter.PhotoImage(file="Python\\Py-Pad\\thenightman.png")
-    about_image_container = customtkinter.CTkLabel(
+    about_image = tk.PhotoImage(file="Python\\Py-Pad\\thenightman.png")
+    about_image_container = ctk.CTkLabel(
         about_window, image=about_image)
     about_image_container.pack(pady="15")
-    about_text = customtkinter.CTkLabel(
+    about_text = ctk.CTkLabel(
         about_window, text_color="black", text="Py Pad designed and written by The Nightman")
     about_text.pack(pady="0")
-    about_text_link = tkinter.Label(
+    about_text_link = tk.Label(
         about_window, fg="blue", cursor="hand2", text="https://github.com/The-Nightman")
     about_text_link.bind(
         "<Button-1>", lambda x: webbrowser.open_new("https://github.com/The-Nightman"))
@@ -148,32 +152,32 @@ def create_about_window():
     about_window.grab_set()
 
 
-customtkinter.set_appearance_mode("light")
+ctk.set_appearance_mode("light")
 
-app = customtkinter.CTk()
+app = ctk.CTk()
 app.geometry(f"{800}x{700}")
 app.title("Untitled - Py Pad")
 
-frame = customtkinter.CTkFrame(app)
+frame = ctk.CTkFrame(app)
 frame.pack(fill="both", expand=True)
 
-text_scroll_y = customtkinter.CTkScrollbar(frame, corner_radius=7)
-text_scroll_x = customtkinter.CTkScrollbar(
+text_scroll_y = ctk.CTkScrollbar(frame, corner_radius=7)
+text_scroll_x = ctk.CTkScrollbar(
     frame, corner_radius=7, orientation="horizontal")
 text_scroll_y.pack(side="right", fill="y")
 text_scroll_x.pack(side="bottom", fill="x")
 
-text_box = tkinter.Text(frame, wrap="none", font=("Helvetica", 11),
-                                    selectbackground="#33b1ff", selectforeground="black", undo=True, yscrollcommand=text_scroll_y.set, xscrollcommand=text_scroll_x.set)
+text_box = tk.Text(frame, wrap="none", font=("Helvetica", 11),
+                   selectbackground="#33b1ff", selectforeground="black", undo=True, yscrollcommand=text_scroll_y.set, xscrollcommand=text_scroll_x.set)
 text_box.pack(fill="both", expand=True)
 
 text_scroll_y.configure(command=text_box.yview)
 text_scroll_x.configure(command=text_box.xview)
 
-menu_widget = tkinter.Menu(app)
+menu_widget = tk.Menu(app)
 app.config(menu=menu_widget)
 
-file_menu = tkinter.Menu(menu_widget, tearoff=False)
+file_menu = tk.Menu(menu_widget, tearoff=False)
 menu_widget.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="New", accelerator="Ctrl+n",
                       command=lambda: new_file())
@@ -186,7 +190,7 @@ file_menu.add_command(label="Save As...",
 file_menu.add_separator()
 file_menu.add_command(label="Exit", command=app.quit)
 
-edit_menu = tkinter.Menu(menu_widget, tearoff=False)
+edit_menu = tk.Menu(menu_widget, tearoff=False)
 menu_widget.add_cascade(label="Edit", menu=edit_menu)
 edit_menu.add_command(label="Undo", accelerator="Ctrl+z",
                       command=text_box.edit_undo)
@@ -201,17 +205,17 @@ edit_menu.add_command(label="Copy", accelerator="Ctrl+c",
 edit_menu.add_command(label="Paste", accelerator="Ctrl+v",
                       command=lambda: paste_text())
 
-format_menu = tkinter.Menu(menu_widget, tearoff=False)
+format_menu = tk.Menu(menu_widget, tearoff=False)
 menu_widget.add_cascade(label="Format", menu=format_menu)
 format_menu.add_checkbutton(label="Text Wrapping",
                             command=lambda: toggle_wrap())
 format_menu.add_command(label="Font...")
 
-help_menu = tkinter.Menu(menu_widget, tearoff=False)
+help_menu = tk.Menu(menu_widget, tearoff=False)
 menu_widget.add_cascade(label="Help", menu=help_menu)
 help_menu.add_command(label="About Py Pad", command=create_about_window)
 
-context_bar = customtkinter.CTkLabel(app, text="test", anchor="e", height=22)
+context_bar = ctk.CTkLabel(app, text="test", anchor="e", height=22)
 context_bar.pack(side="bottom", fill="x")
 
 app.bind("<Control-Key-x>", cut_text)
